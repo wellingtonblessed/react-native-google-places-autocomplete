d11970302751bef70504133c5cf5ec6c7fb9794c
Wellington Mbombi <wellingtonblessed@gmail.com>
2019-12-06 00:24:03 +0100

    keep list in view onblur

--- GooglePlacesAutocomplete.js
+++ GooglePlacesAutocomplete.js
@@ -618,9 +618,9 @@ export default class GooglePlacesAutocomplete extends Component {
   _onBlur = () => {
     this.triggerBlur();
 
-    this.setState({
-      listViewDisplayed: false
-    });
+    // this.setState({
+     //  listViewDisplayed: false
+    // });
   }
 
   _onFocus = () => this.setState({ listViewDisplayed: true })
